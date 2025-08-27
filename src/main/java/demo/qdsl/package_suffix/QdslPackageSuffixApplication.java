ce32e658f801c10b73a337bd93ee5b5bee12a942
CybAtax <62847599+CybAtax@users.noreply.github.com>
2025-08-27 22:52:11 +0200

    Spring initializer

---
+++ src/main/java/demo/qdsl/package_suffix/QdslPackageSuffixApplication.java
@@ -0,0 +1,13 @@
+package demo.qdsl.package_suffix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QdslPackageSuffixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QdslPackageSuffixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/qdsl/package_suffix/QdslPackageSuffixApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.qdsl.package_suffix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QdslPackageSuffixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
